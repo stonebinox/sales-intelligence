86273c9160d9ad15c84a12d3eeec5c80817042ee
Anoop S <anoop.santhanam@gmail.com>
2017-12-25 15:45:28 +0530

    Email count

--- web/index.php
+++ web/index.php
@@ -73,7 +73,7 @@ $app->get("/getEmails",function(Request $request) use($app){
         $service = new Google_Service_Gmail($client);
         $user = 'me';
         $optParams = [];
-        $optParams['maxResults'] = 200; 
+        $optParams['maxResults'] = 100; 
         $optParams['labelIds'] = 'INBOX'; // Only show messages in Inbox
         $messages = $service->users_messages->listUsersMessages('me',$optParams);
         $list = $messages->getMessages();
@@ -149,7 +149,7 @@ $app->get("/getEmails",function(Request $request) use($app){
 
         $user = 'me';
         $optParams = [];
-        $optParams['maxResults'] = 200; 
+        $optParams['maxResults'] = 100; 
         $optParams['labelIds'] = 'SENT'; // Only show messages in Sent
         $messages = $service->users_messages->listUsersMessages('me',$optParams);
         $list = $messages->getMessages();
