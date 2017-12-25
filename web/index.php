d6e3cee07e0d6e7bb1813e02826e51773102565d
Anoop S <anoop.santhanam@gmail.com>
2017-12-25 17:24:12 +0530

    Increased limits

--- web/index.php
+++ web/index.php
@@ -74,7 +74,7 @@ $app->get("/getEmails",function(Request $request) use($app){
         $service = new Google_Service_Gmail($client);
         $user = 'me';
         $optParams = [];
-        $optParams['maxResults'] = 100; 
+        $optParams['maxResults'] = 200; 
         $optParams['labelIds'] = 'INBOX'; // Only show messages in Inbox
         $messages = $service->users_messages->listUsersMessages('me',$optParams);
         $list = $messages->getMessages();
@@ -171,7 +171,7 @@ $app->get("/getEmails",function(Request $request) use($app){
         }
         $user = 'me';
         $optParams = [];
-        $optParams['maxResults'] = 100; 
+        $optParams['maxResults'] = 200; 
         $optParams['labelIds'] = 'SENT'; // Only show messages in Sent
         $messages = $service->users_messages->listUsersMessages('me',$optParams);
         $list = $messages->getMessages();
